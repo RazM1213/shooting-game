b97e8069081bec752f46ba574216bdb16edbc038
Raz Matzliah <razm1213@gmail.com>
2021-09-30 16:53:52 +0300

    added global vars to set the game up

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ from classes.Lives import Lives
 from classes.Player import Player
 from classes.Projectile import Projectile
 pygame.init()
+
+#Setting the game up:
+win = pygame.display.set_mode((500,480))
+pygame.display.set_caption("Shooting Game")
+icon = pygame.image.load(os.path.join('Images', 'standing.png'))
+pygame.display.set_icon(icon)
+score = 0
+lives = 3
+
+clock = pygame.time.Clock()
+
+background = pygame.image.load(os.path.join('Images', 'bg.jpg'))
+char = pygame.image.load(os.path.join('Images', 'standing.png'))
+
+font_gameover = pygame.font.SysFont('comicsans',70,True,True)
+font_score = pygame.font.SysFont('comicsans', 30, True)
+text = font_gameover.render('GAME OVER!', 1, (0,0,0))
+
+background_sound = pygame.mixer.music.load('Sounds/music.mp3')
+pygame.mixer.music.play() #Plays background music
