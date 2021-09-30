2d4c1effa22897fa929e2cf8cd96a7bcb32d8540
Raz Matzliah <razm1213@gmail.com>
2021-09-30 16:39:33 +0300

    set up imports on main game script

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+import os.path
+from classes.Enemy import Enemy
+from classes.Lives import Lives
+from classes.Player import Player
+from classes.Projectile import Projectile
+pygame.init()
